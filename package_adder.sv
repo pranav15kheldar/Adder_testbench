5367d5e2c9409a8cdac0c7ca0b31dacc6d9402a9
Pranav Kheldar <kheldarpranav03@gmail.com>
2025-09-27 18:04:12 +0530

    Monitor.sv is added

--- package_adder.sv
+++ package_adder.sv
@@ -2,6 +2,7 @@ package adder;
     `include "transaction.sv"
     `include "generator.sv"
     `include "driver.sv"
+    `include "monitor.sv"
     `include "enviroment.sv"
     `include "test.sv"
 endpackage
